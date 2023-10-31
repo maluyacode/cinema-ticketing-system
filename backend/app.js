6842dd147f662f1610679b08b4c5fb3a63b8132a
maluyacode <adlawandavemerc98@gmail.com>
2023-10-31 13:32:04 +0800

    express-setup: app.js

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const user = require('./routes/user');
 
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cookieParser());
 
 app.use('/api/v1', user);
 
