964ea459f4f56b5fb015ed661e2a0a662427913e
DahlRoti <joshuavignesh.narayan@tup.edu.ph>
2023-10-31 13:20:56 -0700

    database-setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv')
 dotenv.config({ path: './config/.env' })
-
+const connectDatabase = require('./config/database')
 const app = require('./app')
-
+connectDatabase()
 
 app.listen(process.env.PORT, function () {
     console.log('server started')
