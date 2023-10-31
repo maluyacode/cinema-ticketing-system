4d5535c87005b6025323746bb466e7e2d7946e45
maluyacode <adlawandavemerc98@gmail.com>
2023-10-31 13:28:44 +0800

    cloudinary-setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
+const app = require('./app')
 const dotenv = require('dotenv')
 dotenv.config({ path: './config/.env' })
+const cloudinary = require('cloudinary');
+
 const connectDatabase = require('./config/database')
-const app = require('./app')
+
 connectDatabase()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 app.listen(process.env.PORT, function () {
     console.log('server started')
