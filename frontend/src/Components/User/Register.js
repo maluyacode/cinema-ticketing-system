e3265b0cc014a7181c7b2e20240c43e8fcd73507
DahlRoti <joshuavignesh.narayan@tup.edu.ph>
2023-11-02 10:56:20 -0700

    register-component

---
+++ frontend/src/Components/User/Register.js
@@ -0,0 +1,78 @@
+import React from "react";
+import "./Register.css";
+
+const Register = () => {
+    return (
+        <div className="index">
+            <div className="wireframe">
+                <div className="overlap">
+                    <img
+                        className="vector"
+                        alt="Vector"
+                        src="https://cdn.animaapp.com/projects/6541d4698ff9150ad275896f/releases/6541da40d5437067629b8ebf/img/vector.svg"
+                    />
+                    <img
+                        className="img"
+                        alt="Vector"
+                        src="https://cdn.animaapp.com/projects/6541d4698ff9150ad275896f/releases/6541d4b3677c0dd718b69060/img/vector-1.svg"
+                    />
+                    <img
+                        className="vector-2"
+                        alt="Vector"
+                        src="https://cdn.animaapp.com/projects/6541d4698ff9150ad275896f/releases/6541da40d5437067629b8ebf/img/vector-2.svg"
+                    />
+                    <div className="frame">
+                        <div className="text-wrapper">Create Account</div>
+                        <div className="overlap-group">
+                            <div className="input-frame" />
+                            <div className="div-wrapper">
+                                <div className="div">First Name</div>
+                            </div>
+                        </div>
+                        <div className="overlap-2">
+                            <div className="frame-2" />
+                            <div className="div-wrapper">
+                                <div className="div">Email</div>
+                            </div>
+                        </div>
+                        <div className="overlap-group-2">
+                            <div className="frame-3" />
+                            <div className="frame-4">
+                                <div className="div">Last Name</div>
+                            </div>
+                        </div>
+                        <div className="frame-5">
+                            <div className="text-wrapper-2">Create Account</div>
+                        </div>
+                        <div className="overlap-3">
+                            <div className="frame-6">
+                                <div className="text-wrapper-3">Already have an account?</div>
+                            </div>
+                            <div className="text-wrapper-4">Login</div>
+                        </div>
+                    </div>
+                    <div className="frame-7" />
+                    <div className="frame-8">
+                        <div className="div">Password</div>
+                    </div>
+                    <img
+                        className="untitled-design"
+                        alt="Untitled design"
+                        src="https://cdn.animaapp.com/projects/6541d4698ff9150ad275896f/releases/6541da40d5437067629b8ebf/img/untitled-design-1.png"
+                    />
+                    <img
+                        className="vector-3"
+                        alt="Vector"
+                        src="https://cdn.animaapp.com/projects/6541d4698ff9150ad275896f/releases/6541d4b3677c0dd718b69060/img/vector-3.svg"
+                    />
+                    <p className="explore-the-world-of">
+                        Explore the world <br />
+                        of Blockbusters
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register
