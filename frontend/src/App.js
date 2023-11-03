945f30344ae4d6f30d10e261409793d0ec460322
maluyacode <adlawandavemerc98@gmail.com>
2023-11-03 13:45:19 +0800

    add-header-component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Register from './Components/User/Register'
 import Login from './Components/User/Login'
+import Header from './Components/Layout/Header'
 
 function App() {
   return (
-
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route path='/login' element={<Login />} caseSensitive={true} />
           <Route path='/register' element={<Register />} caseSensitive={true} />
