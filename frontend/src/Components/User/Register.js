9aa36054e074d2ce52bf239f7a41ca1850514b13
airanicoleamodia <airanicole.amodia@tup.edu.ph>
2023-11-03 12:57:46 +0800

    implement-material-ui

--- frontend/src/Components/User/Register.js
+++ frontend/src/Components/User/Register.js
@@ -1,11 +1,22 @@
 import React from "react";
 import './Register.css'
 import { styled } from '@mui/material/styles';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload'
+import { Link as RouterLink } from 'react-router-dom'
+
 import {
     TextField,
     Button,
     Avatar,
+    Container,
+    CssBaseline,
+    Box,
+    Typography,
+    Grid,
+    FormControlLabel,
+    Checkbox,
+    Link
 } from '@mui/material'
 
 const VisuallyHiddenInput = styled('input')({
@@ -21,43 +32,122 @@ const VisuallyHiddenInput = styled('input')({
 });
 
 const Register = () => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
     return (
         <div className="register-container">
             <div className="inner-container">
                 <div className="message-container">
-                    <h2>Explore the world <br /> of Blockbusters</h2>
+                    <h2>Explore the world  of Blockbusters</h2>
                     <div className="image">
-                        <img src="https://cdn.animaapp.com/projects/6541d4698ff9150ad275896f/releases/6541da40d5437067629b8ebf/img/untitled-design-1.png" />
+                        <img src="./images/tape.png" />
                     </div>
                 </div>
                 <div className="create-container">
-                    <div className="form-container">
-                        <h3> Create Account </h3>
-                        <form>
-                            <div className="form-inline">
-                                <TextField label="First Name" variant="outlined" size="small" />
-                                <TextField label="Last Name" variant="outlined" size="small" />
-                            </div>
-                            <div className="form-single">
-                                <TextField label="Email" variant="outlined" className="input-form" size="small" />
-                            </div>
-                            <div className="form-single">
-                                <TextField label="Password" variant="outlined" className="input-form" size="small" />
-                            </div>
-                            <div className="form-single profile">
-                                <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" style={{ marginRight: "5px" }} />
-                                {/* <p className="label-profile">Choose your profile</p> */}
-                                <Button component="label" variant="contained" className="upload-button" startIcon={<CloudUploadIcon />}>
-                                    Upload Photo
-                                    <VisuallyHiddenInput type="file" />
+                    <Container component="main" maxWidth="xs" sx={{ height: 'fit-content' }}>
+                        <CssBaseline />
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Avatar sx={{ m: 1, bgcolor: '#70a0a1' }} src="/static/images/avatar/3.jpg" />
+                            <Typography component="h1" variant="h5">
+                                Create Account
+                            </Typography>
+                            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12} sm={6}>
+                                        <TextField
+                                            autoComplete="given-name"
+                                            name="firstName"
+                                            required
+                                            fullWidth
+                                            id="firstName"
+                                            label="First Name"
+                                            autoFocus
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12} sm={6}>
+                                        <TextField
+                                            required
+                                            fullWidth
+                                            id="lastName"
+                                            label="Last Name"
+                                            name="lastName"
+                                            autoComplete="family-name"
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            required
+                                            fullWidth
+                                            id="email"
+                                            label="Email Address"
+                                            name="email"
+                                            autoComplete="email"
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            required
+                                            fullWidth
+                                            name="password"
+                                            label="Password"
+                                            type="password"
+                                            id="password"
+                                            autoComplete="new-password"
+                                        />
+                                    </Grid>
+                                    <Grid item sm={12}>
+                                        <Button
+                                            component="label"
+                                            variant="outlined"
+                                            startIcon={<CloudUploadIcon />}
+                                            sx={{ color: '#5e6262', borderColor: '#5e6262' }}
+                                        >
+                                            Upload Photo
+                                            <VisuallyHiddenInput type="file" />
+                                        </Button>
+                                    </Grid>
+                                </Grid>
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{
+                                        mt: 3,
+                                        mb: 2,
+                                        backgroundColor: '#70a0a1'
+                                    }}
+                                >
+                                    Sign Up
                                 </Button>
-                            </div>
-                            <Button variant="contained" style={{ backgroundColor: "#527272" }} className="register-button">Create Account</Button>
-                            <div className="text-wrapper-3">Already have an account?
-                                <a href="#" className="login-button" > Login</a>
-                            </div>
-                        </form>
-                    </div>
+                                <Grid container justifyContent="flex-end">
+                                    <Grid item>
+                                        <Link component={RouterLink} to="/login" variant="body2"
+                                            sx={{
+                                                color: '#5e6262',
+                                                textDecoration: 'none'
+                                            }}>
+                                            Already have an account?
+                                            <b> Sign in</b>
+                                        </Link>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </Box>
+                    </Container>
                 </div>
             </div>
         </div>
