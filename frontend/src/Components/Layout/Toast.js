315d227dfe5e112bfcd0fcd01a171c6a121deb20
beajunio <11beajunio@gmail.com>
2023-11-15 23:10:34 +0800

    Layouts/Toast.js

---
+++ frontend/src/Components/Layout/Toast.js
@@ -0,0 +1,46 @@
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+class Toast {
+
+    static success(message) {
+        toast.success(message, {
+            position: "top-center",
+            autoClose: 1500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
+    }
+
+    static error(message) {
+        toast.error(message, {
+            position: "top-center",
+            autoClose: 1500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
+    }
+
+    static warning(message) {
+        toast.warning(message, {
+            position: "top-center",
+            autoClose: 1500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
+    }
+}
+
+export default Toast
