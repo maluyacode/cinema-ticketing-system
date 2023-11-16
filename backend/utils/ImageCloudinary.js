f26dc7e58aa95b50304a6bac75ee7bca17822876
maluyacode <adlawandavemerc98@gmail.com>
2023-11-16 16:44:48 +0800

    apiFeatures, ImageCloudinary, sendEmail utils

---
+++ backend/utils/ImageCloudinary.js
@@ -0,0 +1,46 @@
+const cloudinary = require('cloudinary');
+
+class ImageCloudinary {
+
+    constructor() {
+
+    }
+
+    static async uploadSingle(postImage, folderName) {
+
+        const result = await cloudinary.v2.uploader.upload(postImage, {
+            folder: 'profiles', // folder name in cloudinary, if not exist it will create automatically.
+            width: 200, // convert the width of image to 200 pixel
+            crop: "scale",
+        });
+
+        return {
+            public_id: result.public_id,
+            url: result.secure_url
+        }
+    }
+
+    static async uploadMultiple(postImages, folderName) {
+
+        let images = []
+        for (let i = 0; i < postImages.length; i++) {
+
+            let image = postImages[i].path;
+
+            const result = await cloudinary.v2.uploader.upload(image, {
+                folder: folderName, // folder name in cloudinary, if not exist it will create automatically.
+                width: 200, // convert the width of image to 200 pixel
+                crop: "scale",
+            });
+
+            images.push({
+                public_id: result.public_id,
+                url: result.secure_url
+            })
+        }
+
+        return images
+    }
+}
+
+module.exports = ImageCloudinary;
