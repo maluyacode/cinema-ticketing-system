af6d1537aab37049c89fa6c987355cdc04a30b6a
DahlRoti <joshuavignesh.narayan@tup.edu.ph>
2023-11-21 20:27:47 +0800

    created show crud files

---
+++ frontend/src/Components/Admin/Show/ShowCreate.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowCreate = () => {
+    return (
+        <div>ShowCreate</div>
+    )
+}
+
+export default ShowCreate
---
+++ frontend/src/Components/Admin/Show/ShowsList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowsList = () => {
+    return (
+        <div>ShowsList</div>
+    )
+}
+
+export default ShowsList
